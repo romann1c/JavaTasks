e7777d87e8434e1ef7824175454629f272092ba7
romann1c <36815047+romann1c@users.noreply.github.com>
2022-02-04 22:10:05 +0400

    Update Calculator.java

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -1,4 +1,10 @@
 package com.tsystems.javaschool.tasks.calculator;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+import java.util.Comparator;
+
 
 public class Calculator {
 
