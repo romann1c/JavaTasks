b718d74a15465bee287e6998b5aef5c27faab9bb
romann1c <36815047+romann1c@users.noreply.github.com>
2022-02-04 19:44:25 +0400

    Update PyramidBuilder.java

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -13,8 +13,58 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+
+
+        if (inputNumbers.contains(null)) {
+            throw new CannotBuildPyramidException();
+        }
+
+        int[][] pyramid;
+
+        int size = inputNumbers.size();
+
+        int amountOfNumbersInRow = 0;
+        int rows = 1;
+        int columns = 1;
+
+        while (amountOfNumbersInRow < size) {
+            amountOfNumbersInRow += rows;
+            rows++;
+            columns += 2;
+        }
+        rows--;
+        columns -= 2;
+
+        if (size != amountOfNumbersInRow) {
+            throw new CannotBuildPyramidException();
+        }
+
+        try {
+        inputNumbers.sort(Comparator.naturalOrder());
+        }
+
+        catch (OutOfMemoryError error) {
+            throw new CannotBuildPyramidException();
+        }
+
+        pyramid = new int[rows][columns];
+        for (int[] row : pyramid) {
+            Arrays.fill(row, 0);
+        }
+
+        int middle = (columns / 2);
+        amountOfNumbersInRow = 1;
+        int index = 0;
+
+        for (int i = 0, offset = 0; i < rows; i++, offset++, amountOfNumbersInRow++) {
+            int start = middle - offset;
+            for (int j = 0; j < amountOfNumbersInRow * 2; j += 2, index++) {
+                pyramid[i][start + j] = inputNumbers.get(index);
+            }
+        }
+
+        return pyramid;
+
     }
 
 
