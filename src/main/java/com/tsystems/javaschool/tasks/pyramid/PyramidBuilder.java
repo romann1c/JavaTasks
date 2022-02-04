fcfa7cbc2dbc25c887a4cbe0aeb0ffc7670110bb
romann1c <36815047+romann1c@users.noreply.github.com>
2022-02-04 23:59:41 +0400

    Update PyramidBuilder.java

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -5,29 +5,18 @@ import java.util.List;
 import java.util.List;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import java.util.Stack;
-import java.util.Comparator;
 import java.util.ArrayList;
 
 public class PyramidBuilder {
 
-    /**
-     * Builds a pyramid with sorted values (with minumum value at the top line and maximum at the bottom,
-     * from left to right). All vacant positions in the array are zeros.
-     *
-     * @param inputNumbers to be used in the pyramid
-     * @return 2d array with pyramid inside
-     * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
-     */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
 
 
         if (inputNumbers.contains(null)) {
-            throw new CannotBuildPyramidException();
+            throw new CannotBuildPyramidException();   //Here I'm checking if the input is empty
         }
 
-        int[][] pyramid;
+        int[][] pyramid;                               //"Template" for getting the result ready
 
         int size = inputNumbers.size();
 
@@ -35,39 +24,35 @@ public class PyramidBuilder {
         int rows = 1;
         int columns = 1;
 
-        while (amountOfNumbersInRow < size) {
+        while (amountOfNumbersInRow < size) {         //pure math, the value of number of a row equals to the number of numbers are put in it,
+                                                      //also here I get the number of values needed to fill up the pyramid with specified parameters
             amountOfNumbersInRow += rows;
             rows++;
             columns += 2;
         }
-        rows--;
+        rows--;                                       // these actions are needed to work with indexes
         columns -= 2;
 
-        if (size != amountOfNumbersInRow) {
+        if (size != amountOfNumbersInRow) {           // math again, a pyramid can be built if a number of values can be expressed using the formula "number=n(n+1)/2" 
             throw new CannotBuildPyramidException();
         }
 
-        try {
-        inputNumbers.sort(Comparator.naturalOrder());
-        }
-
-        catch (OutOfMemoryError error) {
-            throw new CannotBuildPyramidException();
-        }
+        
+        Collections.sort(inputNumbers);               //using collections to sort
 
         pyramid = new int[rows][columns];
-        for (int[] row : pyramid) {
+        for (int[] row : pyramid) {                   //fill the "template" up with zeros
             Arrays.fill(row, 0);
         }
 
-        int middle = (columns / 2);
-        amountOfNumbersInRow = 1;
+        int middle = (columns / 2);                  //because top row has only one "block" we need to somehow make the jvm fill the middle of the row,
+        amountOfNumbersInRow = 1;                    //we come to this while we have shifter == 0 and currentRowIndex == 0.            
         int index = 0;
 
-        for (int i = 0, offset = 0; i < rows; i++, offset++, amountOfNumbersInRow++) {
-            int start = middle - offset;
-            for (int j = 0; j < amountOfNumbersInRow * 2; j += 2, index++) {
-                pyramid[i][start + j] = inputNumbers.get(index);
+        for (int i = 0, shifter = 0; i < rows; i++, shifter++, amountOfNumbersInRow++) {
+            int start = middle - shifter;
+            for (int currentRowIndex = 0; currentRowIndex < amountOfNumbersInRow * 2; currentRowIndex += 2, index++) {
+                pyramid[i][start + currentRowIndex] = inputNumbers.get(index);
             }
         }
 
